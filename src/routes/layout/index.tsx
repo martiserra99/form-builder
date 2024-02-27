7d14f28f0169bfe338779ff93013c2931516918a
Marti Serra <martiserramolina99@gmail.com>
2024-02-28 00:57:42 +0100

    Removed header

--- src/routes/layout/index.tsx
+++ src/routes/layout/index.tsx
@@ -2,19 +2,15 @@ import { Outlet } from "react-router-dom";
 
 import { Box, Flex } from "@radix-ui/themes";
 
-import Header from "./components/header";
 import Sidebar from "./components/sidebar";
 
 export default function LayoutRoute() {
   return (
-    <Flex direction="column" className="h-screen">
-      <Header />
-      <Flex direction="row" grow="1">
-        <Sidebar />
-        <Box grow="1" height="100%">
-          <Outlet />
-        </Box>
-      </Flex>
+    <Flex direction="row" className="h-screen">
+      <Sidebar />
+      <Box grow="1" height="100%">
+        <Outlet />
+      </Box>
     </Flex>
   );
 }
