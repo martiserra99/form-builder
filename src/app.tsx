76818ada54e6cd4d295d563d6ad0c87646c6557f
Marti Serra <martiserramolina99@gmail.com>
2024-03-01 13:12:44 +0100

    Changed to create hash router

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import LayoutRoute from "./routes/layout";
 import CreateFormRoute from "./routes/create-form";
@@ -7,7 +7,7 @@ import FormError from "./routes/form/index.error";
 
 import ModifyFormRoute from "./routes/modify-form";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/form-builder",
     element: <LayoutRoute />,
